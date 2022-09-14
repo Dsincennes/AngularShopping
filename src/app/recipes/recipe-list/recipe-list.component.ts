aa5ebae0f0a7e6dee334703a699d0788b2fcceeb
Donald Sincennes <donald.sincennes@jsitelecom.com>
2022-09-14 08:38:00 -0400

    initial Commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Test Description', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimg1'
+    +'.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2F1641226119%2'
+    +'FGettyImages-1156433077.jpg%3Fitok%3DG8JqIVht'),
+    new Recipe('Test Recipe', 'Test Description', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimg1'
+    +'.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2F1641226119%2'
+    +'FGettyImages-1156433077.jpg%3Fitok%3DG8JqIVht')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
